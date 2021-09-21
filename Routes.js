10b4cfe0417486ea3903bbc5a481b73026f8ca97
Alanna <alannapaiva1@hotmail.com>
2021-09-21 15:34:14 -0300

    Create the Routes.js and install some libs

---
+++ Routes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Home from './Pages/Home';
+import Form from './Pages/Form';
+
+const AppStack = createStackNavigator();
+
+const Routes = () => {
+    return (
+        <NavigationContainer>
+            <AppStack.Navigator headerMode="none">
+                <AppStack.Screen name="Home" component={Home} />
+                <AppStack.Screen name="Form" component={Form} />
+            </AppStack.Navigator>
+        </NavigationContainer>
+    );
+}
+
+export default Routes;
